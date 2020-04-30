0464b4f9419853a169d3f6f1be03343a93c7d1cc
ymshenyu <ymshenyu@gmail.com>
2020-04-30 21:58:32 +0800

    use qt5_create_translation instead of using lupdate directly

--- cmake/translations.cmake
+++ cmake/translations.cmake
@@ -2,3 +2,8 @@ find_package(Qt5 COMPONENTS LinguistTools)
 set(TRANSLATIONS_DIR ${CMAKE_SOURCE_DIR}/translations)
 file(GLOB TRANSLATIONS_TS ${TRANSLATIONS_DIR}/**.ts)
 qt5_add_translation(QM_FILES ${TRANSLATIONS_TS})
+qt5_create_translation(TRANSLATIONS_FROM_SOURCES ${QV2RAY_SOURCES} ${TRANSLATIONS_TS} OPTIONS -locations none)
+add_custom_target(lupdate
+    DEPENDS ${TRANSLATIONS_FROM_SOURCES}
+)
+set_target_properties(lupdate PROPERTIES EXCLUDE_FROM_ALL TRUE)
